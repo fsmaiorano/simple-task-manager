32391d9cd96f76c18e10d748a7b098978ffd36c8
Fábio <fsmaiorano@gmail.com>
2019-01-13 16:30:38 -0200

    Rename User for UserRepository

--- TaskManager.Infrastructure.Data/Repositories/UserRepository/UserRespository.cs
+++ TaskManager.Infrastructure.Data/Repositories/UserRepository/UserRespository.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using TaskManager.Domain.Entities;
 using TaskManager.Domain.Interfaces.Repositories;
 using TaskManager.Infrastructure.Data.Context;
 
-namespace TaskManager.Infrastructure.Data.Repositories.User
+namespace TaskManager.Infrastructure.Data.Repositories.UserRepository
 {
-    public class UserRespository: IUserRepository
+    public class UserRespository : IUserRepository
     {
         private readonly TaskManagerContext _context;
         public UserRespository(TaskManagerContext context)
@@ -17,12 +13,12 @@ namespace TaskManager.Infrastructure.Data.Repositories.User
             _context = context;
         }
 
-        public Domain.Entities.User GetById(int id)
+        public User GetById(int id)
         {
-                return _context.Users.Where(x => x.Id.Equals(id)).FirstOrDefault();
+            return _context.Users.Where(x => x.Id.Equals(id)).FirstOrDefault();
         }
 
-        public Domain.Entities.User GetByEmail(string email)
+        public User GetByEmail(string email)
         {
             return _context.Users.Where(x => x.Email.Equals(email)).FirstOrDefault();
         }
