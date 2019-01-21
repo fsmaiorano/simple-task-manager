d72dd89004346aadf4786266f27443e734204316
Fábio <fsmaiorano@gmail.com>
2019-01-21 11:56:05 -0200

    implement signin and add errorMessage component

--- Frontend/src/app/shared/services/auth/auth.service.ts
+++ Frontend/src/app/shared/services/auth/auth.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+
 import { environment } from "src/environments/environment";
+import { Auth } from "../../models/auth.model";
+
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import { ApiResponse } from "../../models/api-response.model";
+import { User } from "../../models/user.model";
 
 @Injectable({
     providedIn: "root"
 })
 export class AuthService {
     constructor(private http: HttpClient) {}
+
+    signin(auth: Auth): Observable<User> {
+        return this.http.post(`${environment.apiUrl}/api/user/signin`, auth).pipe(
+            map((res: ApiResponse) => {
+                if (res.status === 200) {
+                    return res.content as User;
+                }
+                throw new Error(res.message);
+            })
+        );
+    }
 }
